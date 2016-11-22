c92a6af51882f6d88429ce85737eb92e2233dd4d
Ramesh Neupane <ezerames@gmail.com>
2016-11-22 10:50:01 +0545

    pangram check test count

--- pangramchecker.py
+++ pangramchecker.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+
+
+def is_pangram(word):
+    whole_alphabet = set(["a", "b", "c", "d", "e", "f", "g", "h",
+                        "i", "j", "k", "l", "m", "n", "o", "p",
+                        "q", "r", "s", "t", "u", "v", "w", "x",
+                        "y", "z"])
+    
+    alphabet = whole_alphabet
+    isPangram = False
+    pangramCount = 0
+
+    for x in word.lower():
+        if x in alphabet:
+            alphabet.remove(x)
+
+        if not alphabet:
+            isPangram = True
+	    pangramCount += 1
+            alphabet = whole_alphabet
+    
+    return isPangram, pangramCount
+
+
+
+if __name__ == '__main__':
+    main()
