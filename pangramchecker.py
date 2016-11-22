f4dab996ed7c432b408fcde5b64644ffd9695b8e
Ramesh Neupane <ezerames@gmail.com>
2016-11-22 11:50:35 +0545

    pangram checker final

--- pangramchecker.py
+++ pangramchecker.py
@@ -1,27 +1,51 @@
 #!/usr/bin/env python
 
+#function which checks whether the sentence is pangram or not
+#pangram is a sentence that contains all the letters of the English alphabet at least once
+
+#function takes sentence as argument
+#the function returns true is sentence is pangram and its occurence
 
 def is_pangram(word):
-    whole_alphabet = set(["a", "b", "c", "d", "e", "f", "g", "h",
+    
+    alphabet_set = set(["a", "b", "c", "d", "e", "f", "g", "h",
                         "i", "j", "k", "l", "m", "n", "o", "p",
                         "q", "r", "s", "t", "u", "v", "w", "x",
                         "y", "z"])
-    
-    alphabet = whole_alphabet
-    isPangram = False
+
+    isPangram = 'No'
     pangramCount = 0
 
     for x in word.lower():
-        if x in alphabet:
-            alphabet.remove(x)
+        if x in alphabet_set:
+            alphabet_set.remove(x)
+            #remove those alphabets which exists in sentence given 
+        
+        #if alphabet_sets is equal to null then the given words is pangram
 
-        if not alphabet:
-            isPangram = True
+        if not alphabet_set:
+            isPangram = 'Yes'
 	    pangramCount += 1
-            alphabet = whole_alphabet
+            
+            #refill alphabet_set to check again 
+            alphabet_set = set(["a", "b", "c", "d", "e", "f", "g", "h",
+                            "i", "j", "k", "l", "m", "n", "o", "p",
+                            "q", "r", "s", "t", "u", "v", "w", "x",
+                            "y", "z"])
     
     return isPangram, pangramCount
 
+
+def main():
+    
+    test_string = "The quick brown fox jumps over the lazy gdo poiuytrewqasdfghjklmnbvcxz.           The quick brown fox jumps over the lazy dog mnbvcxzasdfghjklpoiuytrewq."
+    
+    isPangram, pangramCount = is_pangram(test_string)
+    print (test_string+'\n')
+
+
+    print (" o Is it a pangram: " + isPangram)
+    print (" o Occurence: " + str(pangramCount))
 
 
 if __name__ == '__main__':
